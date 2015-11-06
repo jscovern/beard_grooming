3cf129c65be2398f455d632c387e6c45d4847cc1
Jamie Scovern <jamie.scovern@gmail.com>
2015-11-05 21:25:56 -0500

    added in some stuff in trimstyles

---
+++ assets/javascript/beardScript.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	console.log("pre-stuff");
+	$(".galleryImage").click(function(){
+		console.log("stuff");
+		// console.log(event);
+		//event.preventDefault();
+
+		// var birthYear=$("#birthYear").val();
+		//var birthYear=$('[name="birthYear"]').val();
+		//console.log(birthYear);
+		//var age=2015-birthYear;
+		//console.log(age);
+		//$("#calculated-age").html("You are "+age+" years old");
+	});
+});
