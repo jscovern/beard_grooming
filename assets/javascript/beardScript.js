4638c9b36b219fe68a96a7848fd7f4b77dab65d0
Jamie Scovern <jamie.scovern@gmail.com>
2015-11-10 19:53:39 -0500

    added links from trimstyles to last

--- assets/javascript/beardScript.js
+++ assets/javascript/beardScript.js
@@ -1,15 +1,23 @@
-$(document).ready(function() {
-	console.log("pre-stuff");
-	$(".galleryImage").click(function(){
-		console.log("stuff");
-		// console.log(event);
-		//event.preventDefault();
+// $(document).ready(function() {
+// 	console.log("pre-stuff");
+// 	$(".galleryImage").click(function(){
+// 		console.log("stuff");
+// 		var picSource=$(this).attr("src");
+// 		console.log(picSource);
+// 		console.log($(".reserveStyle").attr("src"));
+// 		$("last.html").ready(function(){
+// 			console.log("last.html is ready");
+// 			$(".reserveStyle").src=picSource;
+// 		});
+// 		// console.log($(".reserveStyle").src)
+// 		// console.log(event);
+// 		//event.preventDefault();
 
-		// var birthYear=$("#birthYear").val();
-		//var birthYear=$('[name="birthYear"]').val();
-		//console.log(birthYear);
-		//var age=2015-birthYear;
-		//console.log(age);
-		//$("#calculated-age").html("You are "+age+" years old");
-	});
-});
+// 		// var birthYear=$("#birthYear").val();
+// 		//var birthYear=$('[name="birthYear"]').val();
+// 		//console.log(birthYear);
+// 		//var age=2015-birthYear;
+// 		//console.log(age);
+// 		//$("#calculated-age").html("You are "+age+" years old");
+// 	});
+// });
